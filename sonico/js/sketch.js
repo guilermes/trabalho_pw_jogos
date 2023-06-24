fe5dd4044d696ff1012974574d25edf450252f66
Guilherme Nobrega <guilherme.nobreg@hotmail.com>
2023-06-24 17:37:14 -0300

    organizacao2
    organizei algumas coisas nas pastas dos sites

---
+++ sonico/js/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+//  mostraTouch();
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
