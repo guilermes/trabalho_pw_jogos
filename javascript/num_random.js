2635e4bc462b99f36402fbb009d6c4b11fbb3d35
Guilherme Nobrega <guilherme.nobreg@hotmail.com>
2023-06-24 17:12:02 -0300

    atualizacao paginas

--- javascript/num_random.js
+++ javascript/num_random.js
@@ -1,30 +1,63 @@
-//VARIAVEIS
-var numeroSecreto = parseInt(Math.random() * 11);
+document.addEventListener("DOMContentLoaded", function() {
 
-var contador = 0;
+  var numeroSecreto = parseInt(Math.random() * 11);
 
-var resultado = document.getElementById("resultado");
+  var contador = 0;
 
-var revelacao = document.getElementById("revelacao");
+  var resultado = document.getElementById("resultado");
 
-//FUNCOES
-function Chutar() {
-  contador++;
-  var chute = document.getElementById("valor").value;
-  if (chute == numeroSecreto) {
-    resultado.innerHTML  = "Parabéns!Você acertou!";
-    console.log("Deu certo");
-  } else if (chute < numeroSecreto) {
-    console.log("Deu certo");
-    resultado.innerHTML = "Poxa... Você errou.";
-    revelacao.innerHTML = "O número chutado é menor que o número secreto.";
-  } else if (chute > numeroSecreto) {
-    console.log("Deu certo");
-    resultado.innerHTML = "Poxa... Você errou.";
-    revelacao.innerHTML = "O número chutado é maior que o número secreto.";
+  var revelacao = document.getElementById("revelacao");
+
+
+
+
+  function Chutar() {
+
+    contador++;
+
+    var chute = document.getElementById("valor").value;
+
+    if (chute == numeroSecreto) {
+
+      resultado.innerHTML = "Parabéns! Você acertou!";
+
+      console.log("Deu certo");
+
+    } else if (chute < numeroSecreto) {
+
+      console.log("Deu certo");
+
+      resultado.innerHTML = "Poxa... Você errou.";
+
+      revelacao.innerHTML = "O número chutado é menor que o número secreto.";
+
+    } else if (chute > numeroSecreto) {
+
+      console.log("Deu certo");
+
+      resultado.innerHTML = "Poxa... Você errou.";
+
+      revelacao.innerHTML = "O número chutado é maior que o número secreto.";
+
+    }
+
+    if (contador == 3) {
+
+      console.log("Deu certo");
+
+      revelacao.innerHTML = "O número secreto era " + numeroSecreto + ".";
+
+    }
+
   }
-  if (contador == 3) {
-    console.log("Deu certo");
-    revelacao.innerHTML = "O número secreto era " + numeroSecreto + ".";
-  }
-}
+
+
+
+
+  // Associa a função Chutar ao botão de clique
+
+  var button = document.querySelector("button");
+
+  button.addEventListener("click", Chutar);
+
+});
