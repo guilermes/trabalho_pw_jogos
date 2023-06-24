0d274a471a378a1c2a05405515293cd07bd49400
Guilherme Nobrega <guilherme.nobreg@hotmail.com>
2023-06-24 16:55:45 -0300

    atualizacoes

--- javascript/num_random.js
+++ javascript/num_random.js
@@ -12,15 +12,19 @@ function Chutar() {
   contador++;
   var chute = document.getElementById("valor").value;
   if (chute == numeroSecreto) {
-    resultado.innerHTML = "Parabéns!Você acertou!";
+    resultado.innerHTML  = "Parabéns!Você acertou!";
+    console.log("Deu certo");
   } else if (chute < numeroSecreto) {
+    console.log("Deu certo");
     resultado.innerHTML = "Poxa... Você errou.";
     revelacao.innerHTML = "O número chutado é menor que o número secreto.";
   } else if (chute > numeroSecreto) {
+    console.log("Deu certo");
     resultado.innerHTML = "Poxa... Você errou.";
     revelacao.innerHTML = "O número chutado é maior que o número secreto.";
   }
   if (contador == 3) {
+    console.log("Deu certo");
     revelacao.innerHTML = "O número secreto era " + numeroSecreto + ".";
   }
 }
